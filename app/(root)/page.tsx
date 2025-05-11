e66d66bcd1addd8eebff677c917a14c6510ed3ba
Samouil Pratziotis <samouilpratziotis@gmail.com>
2025-05-11 19:59:07 +0300

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,24 @@
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
+import React from "react";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+
+const Home = async () => {
+  const result = await db.select().from(users);
+  console.log(JSON.stringify(result, null, 2));
+
+  return (
+    <>
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName={"mt-28"}
+      />
+    </>
+  );
+};
+
+export default Home;
