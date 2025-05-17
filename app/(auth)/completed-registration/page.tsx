c0759924b1d44810a1b1681e1966cc438c3ebd3d
Samouil Pratziotis <samouilpratziotis@gmail.com>
2025-05-17 21:34:45 +0300

    fix sign-up error

--- app/(auth)/completed-registration/page.tsx
+++ app/(auth)/completed-registration/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import React from "react";
-import AuthForm from "@/components/AuthForm";
-import { signInSchema } from "@/lib/validations";
-import { signInWithCredentials } from "@/lib/actions/auth";
 
 const Page = () => (
   <>
